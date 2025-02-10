173b7f2d7bd4c667bc5fddd1f39dd9a73b6ffc8b
Yako <jakooste@outlook.com>
2025-02-10 14:47:44 +0100

    add home page, rachunek creation functionality

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const mysql = require('mysql2');
-
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'nowy_user',
-  password: 'haslo',
-  database: 'ktokomuiledb'
-})
+const db = require('../db');
 
 const getListaRachunkow = function(req, res, next) {
   db.connect();
-  const zapytanie = 'SELECT nazwa FROM Rachunek';
-  db.query(zapytanie, function(err, results) {
+  const zapytanie = 'SELECT * FROM Rachunek WHERE tworca_id = ?';
+  db.query(zapytanie, [req.user.id], function(err, results) {
     if (err) {
       console.error('Error in getListaRachunkow:', err);
       return res.status(500).send('Database error');
@@ -32,7 +26,35 @@ const getGlobalInfo = function(req, res, next) {
 router.get('/', function(req, res) {
   console.log('Home page, user:', req.user);
   console.log('Authenticated:', req.isAuthenticated());
-  res.render('index', {authentication: req.isAuthenticated(), user: req.user });
+
+  if(req.isAuthenticated()){
+    console.log('User' + req.user.username + 'logged in, redirecting');
+    res.redirect('/home');
+  }
+  else {
+    console.log('Guest user, redirecting to login');
+    res.redirect('/login');
+  }
 });
+
+router.get('/home', getListaRachunkow, function(req, res) {
+  console.log('Home page, user:', req.user);
+  console.log('Rachunki:', req.rachunki);
+  res.render('home', {user: req.user, rachunki: req.rachunki});
+})
+
+router.get('/home/new', function(req, res) {
+  res.render('addRachunek');
+})
+
+router.post('/home/new', function(req, res) {
+  console.log('Creating new Rachunek "' + req.body.nazwa +'"');
+  db.connect();
+  const zapytanie = 'INSERT INTO Rachunek(tworca_id, nazwa) VALUES (?,?)';
+  db.query(zapytanie, [req.user.id, req.body.nazwa], function(err, results) {
+    console.log('Created');
+    res.redirect('/home');
+  })
+})
 
 module.exports = router;
