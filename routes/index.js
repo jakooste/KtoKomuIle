1499711e444efd47df51334e38f3f85de4c76a55
Yako <jakooste@outlook.com>
2025-02-10 21:32:28 +0100

    add Wydatki view

--- routes/index.js
+++ routes/index.js
@@ -15,11 +15,35 @@ const getListaRachunkow = function(req, res, next) {
     next()
   })
 }
-
-const global_info = {title: 'KtoKomuIle'};
-const getGlobalInfo = function(req, res, next) {
-  req.global_info = global_info;
-  next();
+const authRachunekAccess = function (req, res, next) {
+  db.connect();
+  console.log('Authorising access to Rachunek', req.params.idRachunek);
+  const zapytanie2 = `SELECT * FROM Rachunek WHERE idRachunek = ? AND tworca_id = ?`;
+  db.query(zapytanie2, [req.params.idRachunek, req.user.id], function(err, results) {
+    console.log('Znaleziony rachunek: ', results[0]);
+    if (err) {
+      console.log('Error in authRachunekAccess:', err);
+      return res.status(500).send('Database error');
+    }
+    req.isRachunekAuthenthicated = results.length > 0;
+    if (!req.isRachunekAuthenthicated) {
+      res.redirect('/rachunek/'+ req.params.idRachunek + '/notfound');
+    }
+    req.rachunek = results[0];
+    next();
+  })
+}
+const getListaWydatkow = function (req, res, next) {
+  db.connect();
+  const zapytanie = `SELECT * FROM Wydatki WHERE idRachunek = ?`;
+  db.query(zapytanie, [req.params.idRachunek], function(err, results) {
+    if (err) {
+      console.error('Error in getListaWydatkow:', err);
+      return res.status(500).send('Database error');
+    }
+    req.wydatki = results;
+    next();
+  })
 }
 
 /* GET home page. */
@@ -55,6 +79,20 @@ router.post('/home/new', function(req, res) {
     console.log('Created');
     res.redirect('/home');
   })
+})
+
+router.get('/rachunek/:idRachunek', function(req, res) {
+  res.redirect('/rachunek/' + req.params.idRachunek + '/wydatki');
+})
+
+router.get('/rachunek/:idRachunek/wydatki/', authRachunekAccess, getListaWydatkow, function(req, res) {
+  console.log('Rachunek:', req.rachunek);
+  console.log('Wydatki:', req.wydatki);
+  res.render('rachunekWydatki', {user: req.user, rachunek: req.rachunek, wydatki: req.wydatki});
+})
+
+router.get('/rachunek/:idRachunek/notfound', function(req, res) {
+  res.render('notfoundRachunek');
 })
 
 module.exports = router;
